78ba7bd53bb7c116f85d04a11d8a06dd773a1099
Daniel Zoba <daniel.sobe@sorben.com>
2022-11-07 09:43:28 +0100

    easily tag containers for possible rollback

---
+++ run_this_to_tag_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONTAINERVERSION=custom-0.0.2
+
+docker image tag workadventure-front-custom:latest  workadventure-front-custom:$CONTAINERVERSION
+docker image tag workadventure-back-custom:latest   workadventure-back-custom:$CONTAINERVERSION
+docker image tag workadventure-pusher-custom:latest workadventure-pusher-custom:$CONTAINERVERSION
+
