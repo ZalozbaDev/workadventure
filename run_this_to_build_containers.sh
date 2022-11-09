e00203a0e99655f656309bb2f53dae0b56c10205
Daniel Zoba <daniel.sobe@sorben.com>
2022-11-09 17:11:18 +0100

    build chat container from sources as well

--- run_this_to_build_containers.sh
+++ run_this_to_build_containers.sh
@@ -18,6 +18,14 @@ echo "#########################################################"
 
 docker buildx build -t workadventure-back-custom -f back/Dockerfile .
 
+echo "#########################################################"
+echo "################ CHAT ###################################"
+echo "#########################################################"
+
+docker buildx build -t workadventure-chat-custom -f front/chat/Dockerfile .
+
+
+
 # using this bash line to compute words-to-translate
 # for i in $(find front/src/i18n/de-DE/ -name "*.ts"); do cat $i | grep '"' | grep -v import | grep -v const | sed -e s/.*\:// ; done  | wc
 
--- run_this_to_tag_containers.sh
+++ run_this_to_tag_containers.sh
@@ -5,4 +5,5 @@ CONTAINERVERSION=custom-0.0.2
 docker image tag workadventure-front-custom:latest  workadventure-front-custom:$CONTAINERVERSION
 docker image tag workadventure-back-custom:latest   workadventure-back-custom:$CONTAINERVERSION
 docker image tag workadventure-pusher-custom:latest workadventure-pusher-custom:$CONTAINERVERSION
+docker image tag workadventure-chat-custom:latest   workadventure-chat-custom:$CONTAINERVERSION
 
