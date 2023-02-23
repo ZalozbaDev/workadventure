165b6a5da72124ccfe5763e76d149a08d35680ec
Daniel Zoba <daniel.sobe@sorben.com>
2023-02-23 11:38:15 +0100

    remove old files

--- front/src/i18n/hsb-DE/notification.ts
+++
@@ -1,9 +0,0 @@
-import { DeepPartial } from "../../Utils/DeepPartial";
-import type { Translation } from "../i18n-types";
-
-const notification: DeepPartial<Translation["notification"]> = {
-    discussion: "chce z tobu diskutować",
-    message: "sćele Wam powěsć",
-};
-
-export default notification;
